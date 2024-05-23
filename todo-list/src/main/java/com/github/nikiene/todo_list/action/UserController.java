a4c55e7393f15dc6ab81b22a3f395c52f54eed02
nikiene <nikolasnovo03@gmail.com>
2024-05-23 13:14:32 -0300

    feat: hash user password

--- todo-list/src/main/java/com/github/nikiene/todo_list/action/UserController.java
+++ todo-list/src/main/java/com/github/nikiene/todo_list/action/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.nikiene.todo_list.model.UserModel;
 import com.github.nikiene.todo_list.repositories.IUserRepository;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +29,10 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
         }
 
+        BCrypt.Hasher hasher = BCrypt.withDefaults();
+        var hashedPassword = hasher.hashToString(12, user.getPassword().toCharArray());
+
+        user.setPassword(hashedPassword);
         var createdUser = this.userRepository.save(user);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
