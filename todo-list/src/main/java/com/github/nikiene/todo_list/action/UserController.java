ba9844c731757396ea79fbeb3e8d6e3dbbe070f5
nikiene <nikolasnovo03@gmail.com>
2024-05-23 20:06:19 -0300

    chore: Remove unused getUserById endpoint in UserController

--- todo-list/src/main/java/com/github/nikiene/todo_list/action/UserController.java
+++ todo-list/src/main/java/com/github/nikiene/todo_list/action/UserController.java
@@ -1,6 +1,5 @@
 package com.github.nikiene.todo_list.action;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +9,6 @@ import com.github.nikiene.todo_list.model.UserModel;
 import com.github.nikiene.todo_list.repositories.IUserRepository;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,17 +20,6 @@ public class UserController {
 
     @Autowired
     private IUserRepository userRepository;
-
-    @GetMapping("")
-    public ResponseEntity<Object> getUserById(UUID id) {
-        var user = this.userRepository.findById(id);
-
-        if (user.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body(user.get());
-    }
 
     @PostMapping("")
     public ResponseEntity<Object> postUser(@RequestBody UserModel user) {
