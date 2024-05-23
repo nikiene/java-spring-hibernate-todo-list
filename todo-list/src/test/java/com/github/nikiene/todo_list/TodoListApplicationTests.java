a7263530573454732c644b69af7f318d4eb806ed
nikiene <nikolasnovo03@gmail.com>
2024-05-23 10:44:53 -0300

    chore: Add initial project files and configuration

---
+++ todo-list/src/test/java/com/github/nikiene/todo_list/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nikiene.todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
